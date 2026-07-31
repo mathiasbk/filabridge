713ff2dbc2c59e1a144dc976d4395eb63141d5c0
mathiasbk <619684+mathiasbk@users.noreply.github.com>
2026-07-31 20:30:46 +0200

    Removed debug Print() statement

--- web.go
+++ web.go
@@ -598,7 +598,6 @@ func (ws *WebServer) updateConfigHandler(c *gin.Context) {
 
 	// Update each config value
 	for key, value := range config {
-		fmt.Print("test: " + value)
 		//Remoive trailing slash /
 		value = strings.TrimSuffix(value, "/")
 		if err := ws.bridge.SetConfigValue(key, value); err != nil {
